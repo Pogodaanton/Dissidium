1cf576acc0667da75375df0c22f71704a68d3584
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2020-12-22 19:46:40 +0100

    Find channel util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Guild, Message, TextChannel } from "discord.js";
 
 export const sendError = (
   message: Message,
@@ -8,4 +8,15 @@ export const sendError = (
   message.channel.send(`:x: ${errorMessage}`).then(msg => {
     if (cooldown > 0 && msg.deletable) msg.delete({ timeout: cooldown * 1000 });
   });
+};
+
+export const findChannel = (guild: Guild | null, channelName: string): TextChannel => {
+  const foundChannel = guild?.channels.cache.find(
+    chn => chn.name === channelName && chn.type === "text"
+  );
+
+  if (!foundChannel || typeof foundChannel === "undefined")
+    throw new Error("Channel with the given name could not be found.");
+
+  return foundChannel as TextChannel;
 };
