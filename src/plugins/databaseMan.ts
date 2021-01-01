6ed3d07bc528be09d986b5f739a80156eb499a71
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2021-01-01 17:37:07 +0100

    Cleanup, GuildID support, added getAllGuilds

--- src/plugins/databaseMan.ts
+++ src/plugins/databaseMan.ts
@@ -1,69 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/*
-import lowdb from "lowdb";
-import FileAsync from "lowdb/adapters/FileAsync";
-import Plugin from "../PluginStructs";
-import { resolve } from "path";
-import { Guild } from "discord.js";
-import _ from "lodash";
-
-const dbPath = resolve(__dirname, "../../db/db.json");
-
-/*
-db.defaults({ posts: [] })
-  .write()
-
-const result = db.get('posts')
-  .push({ title: process.argv[2] })
-  .write()
-
-console.log(result)
-
-
-interface GuildDB {
-  bouncer?: {
-    langRoles?: {
-      [emoji: string]: string;
-    };
-  };
-  masters?: string[];
-}
-
-/**
- * Handles database i/O
- *
-export default class DatabaseMan extends Plugin {
-  db: lowdb.LowdbAsync<any> | null = null;
-
-  getDB = (): lowdb.LowdbAsync<any> | null => this.db;
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  getGuildDB = (guild: Guild | null): GuildDB | null => {
-    if (!this.db || !guild) return null;
-    const guildObj = this.db.get("guilds." + guild.id).value();
-
-    if (typeof guildObj !== "object") return {};
-    return guildObj;
-  };
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  setGuildDB = (guild: Guild | null, obj: GuildDB): void => {
-    if (!this.db || !guild) return;
-    this.db.set("guilds." + guild.id, obj).write();
-  };
-
-  load = async (): Promise<void> => {
-    const adapter = new FileAsync(dbPath);
-    this.db = await lowdb(adapter);
-  };
-
-  unload = async (): Promise<void> => {
-    this.db?.write();
-  };
-}
-
-*/
-
 import Plugin from "../utils/PluginStructs";
 import { JsonDB } from "node-json-db";
 import { Config } from "node-json-db/dist/lib/JsonDBConfig";
@@ -72,6 +7,8 @@ import { Guild } from "discord.js";
 
 const dbPath = resolve(__dirname, "../../db/db.json");
 const dbConfig = new Config(dbPath, true, false, "/");
+
+export type GuildOrGuildID = Guild | string | null;
 
 export default class DatabaseMan extends Plugin {
   db: JsonDB = new JsonDB(dbConfig);
@@ -83,10 +20,17 @@ export default class DatabaseMan extends Plugin {
    *
    * @memberof DatabaseMan
    */
-  checkGuild = (guild: Guild | null): guild is Guild => {
-    if (!guild) throw new Error("No Guild object given!");
+  private checkGuild = (guild: Guild | string | null): guild is Guild | string => {
+    if (!guild) throw new Error("DatabaseMan only works in guilds!");
     return true;
   };
+
+  /**
+   * Retrieves the guild id from your input
+   * @param guild The guild you need the ID of
+   */
+  private getGuildID = (guild: Guild | string): string =>
+    typeof guild === "string" ? guild : guild.id;
 
   /**
    * Gets data from a specific guild
@@ -95,12 +39,12 @@ export default class DatabaseMan extends Plugin {
    * @param path The path to the data you want to access seperated with `/`
    * @memberof DatabaseMan
    */
-  getGuildData = async <T>(guild: Guild | null, path: string): Promise<T | null> => {
+  getGuildData = async <T>(guild: GuildOrGuildID, path: string): Promise<T | null> => {
     if (!this.checkGuild(guild)) return null;
 
     try {
       const data = this.db.getData(
-        `/${guild.id}${path.startsWith("/") ? "" : "/"}${path}`
+        `/${this.getGuildID(guild)}${path.startsWith("/") ? "" : "/"}${path}`
       );
       return data;
     } catch (err) {
@@ -125,14 +69,14 @@ export default class DatabaseMan extends Plugin {
    * @memberof DatabaseMan
    */
   getGuildDataIndex = async (
-    guild: Guild | null,
+    guild: GuildOrGuildID,
     path: string,
     needle: string,
     needleKey?: string
   ): Promise<number> => {
     if (!this.checkGuild(guild)) return -1;
     return this.db.getIndex(
-      `/${guild.id}${path.startsWith("/") ? "" : "/"}${path}`,
+      `/${this.getGuildID(guild)}${path.startsWith("/") ? "" : "/"}${path}`,
       needle,
       needleKey
     );
@@ -148,14 +92,14 @@ export default class DatabaseMan extends Plugin {
    * @memberof DatabaseMan
    */
   setGuildData = async <T>(
-    guild: Guild | null,
+    guild: GuildOrGuildID,
     path: string,
     data: T,
     override = false
   ): Promise<void> => {
     if (!this.checkGuild(guild)) return;
     return this.db.push(
-      `/${guild.id}${path.startsWith("/") ? "" : "/"}${path}`,
+      `/${this.getGuildID(guild)}${path.startsWith("/") ? "" : "/"}${path}`,
       data,
       override
     );
@@ -171,13 +115,13 @@ export default class DatabaseMan extends Plugin {
    * @memberof DatabaseMan
    */
   deleteGuildData = async <T>(
-    guild: Guild | null,
+    guild: GuildOrGuildID,
     path: string,
     arrayNeedle?: T,
     arrayNeedleKey?: string
   ): Promise<void> => {
     if (!this.checkGuild(guild)) return;
-    const pathPrefix = `/${guild.id}${path.startsWith("/") ? "" : "/"}`;
+    const pathPrefix = `/${this.getGuildID(guild)}${path.startsWith("/") ? "" : "/"}`;
 
     if (arrayNeedle) {
       const index = this.db.getIndex(pathPrefix + path, arrayNeedle + "", arrayNeedleKey);
@@ -185,6 +129,40 @@ export default class DatabaseMan extends Plugin {
     }
 
     return this.db.delete(pathPrefix + path);
+  };
+
+  /**
+   * Retrieves the id of every guild the bot has ever interacted with.
+   * @returns Promise<string[]>
+   */
+  getAllGuildIDs = async (): Promise<string[]> => {
+    const obj = (await this.db.getData("/")) as { [guildID: string]: any };
+    if (!obj) return [];
+
+    return Object.keys(obj);
+  };
+
+  /**
+   * Retrieves the guilds the bot has interacted with at least once.
+   * Note that guilds with no corresponding Guild class will not be included in the returned array.
+   *
+   * @returns Promise<Guild[]>
+   */
+  getAllGuilds = async (): Promise<Guild[]> => {
+    const guildIDs = await this.getAllGuildIDs();
+    const guilds: Guild[] = [];
+    guildIDs.forEach(async id => {
+      try {
+        const guild = await this.client.guilds.fetch(id);
+        if (!guild) return;
+        guilds.push(guild);
+      } catch (err) {
+        console.error("[Guild retrieval failed]", err);
+        return;
+      }
+    });
+
+    return guilds;
   };
 
   load = (): void => {
